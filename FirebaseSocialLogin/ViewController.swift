56de54ff1e25f37fc36385a3ab3689e6b60adba9
se7en <ngocthaicao@gmail.com>
2017-04-17 14:52:06 +0800

    Initial Commit

---
+++ FirebaseSocialLogin/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FirebaseSocialLogin
+//
+//  Created by Thai Cao Ngoc on 17/4/17.
+//  Copyright © 2017 Harry. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
